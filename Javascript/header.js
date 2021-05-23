63f3733150eb5edae83726ccf886b95a30618dcd
Florence-tsou <c6i0n2d2y5@gmail.com>
2021-05-23 19:51:10 +0800

    0523

---
+++ Javascript/header.js
@@ -0,0 +1,7 @@
+var hamburger = document.getElementById("hamburger");
+var open = document.getElementById("hamburger_open");
+
+
+hamburger.addEventListener("click", function(){
+  open.classList.toggle("test");
+});
