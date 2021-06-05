a850d8a6e5b685a373f411eafbcb15bf63115894
Florence-tsou <c6i0n2d2y5@gmail.com>
2021-06-05 23:27:29 +0800

    0605

--- Javascript/item.js
+++ Javascript/item.js
@@ -1,3 +1,5 @@
+
+// 數量增減
 var add = document.getElementById("plus");
 var minus = document.getElementById("minus");
 var abc = document.getElementById("abc");
@@ -15,5 +17,21 @@ minus.addEventListener("click", function(){
 });
 
 
+// 切換選單
 var nioi = document.getElementById("nioi");
 var use = document.getElementById("use");
+var text = document.getElementById("text");
+var use_method = document.getElementById("use_method");
+
+function open(item, item2, item3, item4){
+  item.addEventListener("click", function(e){
+    e.preventDefault();
+    e.target.classList.add("-on");
+    item2.classList.remove("-on");
+    item3.style.display = "block";
+    item4.style.display = "none";
+  });
+}
+
+open(nioi, use, text, use_method);
+open(use, nioi, use_method, text);
