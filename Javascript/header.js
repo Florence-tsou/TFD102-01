548b121b87d6e090e1d6171cd8c68d8ec2428cfe
Florence-tsou <c6i0n2d2y5@gmail.com>
2021-06-08 20:22:16 +0800

    0608

--- Javascript/header.js
+++ Javascript/header.js
@@ -1,3 +1,4 @@
+// 漢堡選單
 var hamburger = document.getElementById("hamburger");
 var open_1 = document.getElementById("hamburger_open");
 
@@ -5,3 +6,18 @@ var open_1 = document.getElementById("hamburger_open");
 hamburger.addEventListener("click", function(){
   open_1.classList.toggle("test");
 });
+
+
+
+// 購物車刪除
+
+var del_btn = document.getElementsByClassName("garbage");
+
+for(let i = 0; i < del_btn.length; i++){
+  del_btn[i].addEventListener("click", function(e){
+    e.target.closest("tr.cart_list_item").classList.add("zz");
+    setTimeout(function(){
+      e.target.closest("tr.cart_list_item").remove();
+    }, 1000);
+  });
+};
